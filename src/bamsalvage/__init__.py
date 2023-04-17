fab9de1a40b18c7fb7d55e4735a06c772a8e7c18
Takaho A. Endo <takaho.endo@riekn.jp>
2023-04-17 15:30:35 +0900

    pyproject.toml

--- src/bamsalvage/__init__.py
+++ src/bamsalvage/__init__.py
@@ -1,12 +1,14 @@
 """bamsalvage, retrieving tool of (especially long) reads from BAM files."""
 
-"""
+#print('BAMSALVAGE: Imported {}'.format(__file__))
+
+from bamsalvage._bamsalvage import *
+
 try:
     __version__ = version(__name__)
 except Exception:
+    __version__="0.1.2"
     pass
-"""
-__version__="0.1.2"
 
-from _bamsalvage import *
+# from _bamsalvage import *
     
